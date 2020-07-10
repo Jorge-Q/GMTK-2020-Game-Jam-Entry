0244e5fb6b737af1e2f30f23ec074f0051c75e9f
Ivan Mota <ivanmota@iMac.local>
2020-07-10 17:08:03 -0600

    boiler plate code

--- src/Main.js
+++ src/Main.js
@@ -1,22 +1,30 @@
 let frame = document.getElementById("frame");
 let graphic = frame.getContext("2d");
 
+let sprite = new Image();
+sprite.src = "./sprites/mario.png";
+
+let x = 0;
+let x_speed = 10;
 
 window.onload = ()=>{
     requestAnimationFrame(gameLoop)
 }
 
 function gameLoop(){
-
     update()
     render()
     requestAnimationFrame(gameLoop)
 }
 
 function update(){
-    console.log("update")
+    x += x_speed;
+    if(x > frame.width){
+        x_speed = -x_speed;
+    }
 }
 
 function render(){
-    console.log("render")
+    graphic.clearRect(0, 0, frame.width, frame.height);
+    graphic.drawImage(sprite, x, 100, 20, 20);
 }
