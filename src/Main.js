7d8c03c7768daf6251f91289311d301ecb16f665
Jorge-Q <j.quinonez.980@gmail.com>
2020-07-10 17:27:49 -0600

    Added boundaries

--- src/Main.js
+++ src/Main.js
@@ -6,6 +6,8 @@ sprite.src = "./sprites/mario.png";
 
 let x = 0;
 let x_speed = 10;
+let y = 0;
+let y_speed = 10;
 
 window.onload = ()=>{
     requestAnimationFrame(gameLoop)
@@ -19,8 +21,18 @@ function gameLoop(){
 
 function update(){
     x += x_speed;
+    y += y_speed;
     if(x > frame.width){
         x_speed = -x_speed;
+    }
+    if(x < 0){
+        x_speed = -x_speed;
+    }
+    if(y > frame.width){
+        y_speed = -y_speed;
+    }
+    if(y < 0){
+        y_speed = -y_speed;
     }
 }
 
