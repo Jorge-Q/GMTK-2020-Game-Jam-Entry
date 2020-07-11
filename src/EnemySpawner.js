c193067291d7b66ad0882567d5b66a3793a30ac8
Jorge-Q <j.quinonez.980@gmail.com>
2020-07-11 17:15:12 -0600

    Made enemies spawn at lower rate

--- src/EnemySpawner.js
+++ src/EnemySpawner.js
@@ -4,12 +4,12 @@ class EnemySpawner extends Entity{
         super(x, y, width, height);
         this.sprite = new Image();
         this.sprite.src = "./sprites/spaceship.png";
-        this.cooldown = 60;
+        this.cooldown = 120;
     }
 
     update(){
         if(this.cooldown <= 0){
-            this.cooldown = 60;
+            this.cooldown = 120;
             this.spawnEnemy();
         }
         this.cooldown --;
