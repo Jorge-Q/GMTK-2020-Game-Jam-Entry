471d328af7c998230a04a0ca4da32703550e0421
Ivan Mota <ivanmota@iMac.local>
2020-07-10 23:52:04 -0600

    Added sprites

--- src/Enemy.js
+++ src/Enemy.js
@@ -9,7 +9,20 @@ class Enemy extends Entity{
     update(){
         super.update();
         let angle = -Math.atan2(player.y - player.height - this.y, player.x - player.width - this.x);
-        this.xVelocity = Math.cos(angle) * 1;
-        this.yVelocity = -Math.sin(angle) * 1;
+        this.xVelocity = Math.cos(angle) * .75;
+        this.yVelocity = -Math.sin(angle) * .75;
+        this.checkBulletCollision();
+    }
+
+    checkBulletCollision(){
+        for(let i = 0; i < player.bullets.length; i++){
+            if(this.x < player.bullets[i].x + player.bullets[i].width &&
+                this.x + this.width > player.bullets[i].x &&
+                this.y < player.bullets[i].y + player.bullets[i].height &&
+                this.y + this.height > player.bullets[i].y){
+                enemies.splice(this, 1);
+                player.bullets.splice(i, 1);
+            }
+        }
     }
 }
